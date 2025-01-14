eed41b10da1556b3fd507e9e0c9f6598a789c962
Daffa Rizq <daimenrizq7@gmail.com>
2025-01-14 11:39:35 +0700

    feat: initial commit

---
+++ src/main.ts
@@ -0,0 +1,54 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { Routes } from "./interface/routes.interface";
+import { errorMiddleware } from "./middlewares/error.middleware";
+
+dotenv.config();
+
+class App {
+  public app: express.Application;
+  public env: string;
+  public port: string | number;
+
+  constructor(routes: Routes[]) {
+    this.app = express();
+    this.env = process.env.NODE_ENV || "development";
+    this.port = process.env.PORT || 3000;
+
+    this.initializeMiddlewares();
+    this.initializeRoutes(routes);
+    this.initializeErrorHandling();
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(
+        `[BTS.id] Server is running at http://localhost:${this.port}`
+      );
+    });
+  }
+
+  public getServer() {
+    return this.app;
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  private initializeRoutes(routes: Routes[]) {
+    routes.forEach((route) => {
+      this.app.use("/", route.router);
+    });
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
+  }
+}
+
+export default App;
